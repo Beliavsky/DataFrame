1c7557672b5350672bf368addd0adc068448997c
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2025-02-25 19:13:57 -0500

    fixed value of psi_half

--- constants.f90
+++ constants.f90
@@ -10,5 +10,5 @@ real(kind=dp), parameter :: &
    sqrt_two   = 1.4142135623730951_dp, &
    euler      = 0.5772156649015328606065120900824024_dp, &
    polygamma_one_half = 4.93480220054468_dp ! polygamma(1, 0.5), &
-   psi_half = 4.93480220054468_dp
+   psi_half = -1.9635100260214235_dp
 end module constants_mod
