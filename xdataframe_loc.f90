3ce2c8726b1bfaf9127aeec06a7ac6af56b83875
Beliavsky <beliavsky@aol.com>
2025-03-21 09:16:58 -0400

    added titles to displays

--- xdataframe_loc.f90
+++ xdataframe_loc.f90
@@ -1,13 +1,16 @@
 program xdataframe_loc
 use dataframe_mod, only: DataFrame, display, random
 type(DataFrame) :: df
-call random(df, 5, 4)
+call random(df, 4, 3)
 df%index = df%index * 10
-call display(df, title="df", fmt_trailer="()")
-call display(df%icol([2,3]), title="df%icol([2,3]", fmt_trailer="()")
+call display(df, title="df")
+! subset by row and column numbers
+call display(df%icol([2,3]), title="df%icol([1,3])")
 call display(df%irow([2,4]), title="df%irow([2,4])")
-call display(df%loc(rows=[20, 40]))
-call display(df%loc(columns=["C2", "C3"]))
-call display(df%loc(rows=[20, 40], columns=["C2", "C3"]))
+! subset by index values and column names
+call display(df%loc(rows=[20, 40]), title="df%loc(rows=[20, 40]")
+call display(df%loc(columns=["C1", "C3"]), title="df%loc(columns=['C1', 'C3'])")
+call display(df%loc(rows=[20, 40], columns=["C1", "C3"]), &
+             title="df%loc(rows=[20, 40], columns=['C1', 'C3']")
 call display(df%loc())
 end program xdataframe_loc
