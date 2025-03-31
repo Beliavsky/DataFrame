5bde364eb708d4278b62fd348ea617636fd42eb0
Beliavsky <beliavsky@aol.com>
2025-03-30 20:39:08 -0400

    added function cor()

--- dataframe_stats.f90
+++ dataframe_stats.f90
@@ -1,18 +1,28 @@
 module dataframe_stats_mod
-use kind_mod, only: dp
-use dataframe_mod, only: DataFrame, nrow
-use table_mod
+use kind_mod       , only: dp
+use dataframe_mod  , only: DataFrame, nrow
+use table_mod      , only: Table
 use table_stats_mod, only: stats_table
-use basic_stats_mod, only: stats
+use basic_stats_mod, only: stats, corr_mat
 implicit none
 private
-public :: simple_ret, moving_sum, moving_average, stats ! , stats_df
+public :: simple_ret, moving_sum, moving_average, stats, cor
 interface stats
    module procedure stats_df
 end interface stats
 contains
 
+elemental function cor(df) result(xtable)
+! return correlation matrix of the columns of a dataframe
+type(DataFrame), intent(in) :: df
+type(Table)                 :: xtable
+xtable = Table(index=df%columns, columns=df%columns, &
+   values=corr_mat(df%values))
+xtable%index = df%columns
+end function cor
+
 function stats_df(funcs, df) result(xtable)
+! return statistics on each column of a dataframe
 character (len=*), intent(in) :: funcs(:)
 type(DataFrame), intent(in) :: df
 type(Table)                 :: xtable
@@ -20,6 +30,7 @@ xtable = stats_table(funcs, df%values, columns=df%columns)
 end function stats_df
 
 function simple_ret(df) result(df_ret)
+! return simple returns (proportional changes) for each column of a dataframe
 type(DataFrame), intent(in) :: df
 type(DataFrame)             :: df_ret
 integer                     :: nr
@@ -32,6 +43,7 @@ end if
 end function simple_ret
 
 function moving_sum(df, nterms) result(df_sum)
+! return the moving sums of the columns of a dataframe
 type(DataFrame), intent(in) :: df
 integer        , intent(in) :: nterms
 type(DataFrame)             :: df_sum
@@ -45,6 +57,7 @@ end do
 end function moving_sum
 
 function moving_average(df, nterms) result(df_sum)
+! return the moving averages of the columns of a dataframe
 type(DataFrame), intent(in) :: df
 integer        , intent(in) :: nterms
 type(DataFrame)             :: df_sum
