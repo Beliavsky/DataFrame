62e3e035ca959e3907eb611739d98b1c01179a6f
Beliavsky <beliavsky@aol.com>
2025-04-05 12:37:24 -0400

    reduced code, still demonstrates bug

--- x1bug.f90
+++ x1bug.f90
@@ -1,7 +1,5 @@
-module basic_stats_mod
+module m1_mod
 implicit none
-private
-public :: foo
 interface foo
    module procedure foo_mat
 end interface foo
@@ -13,30 +11,28 @@ integer, intent(in) :: nacf
 character (len=*), intent(in), optional :: label
 end subroutine foo_mat
 
-end module basic_stats_mod
+end module m1_mod
 
-module dataframe_stats_mod
-use basic_stats_mod, only: foo
+module m2_mod
+use m1_mod, only: foo
 implicit none
-private
-public :: foo
 interface foo
   module procedure foo_df
 end interface foo
 contains
 
-impure elemental subroutine foo_df(nacf, outu, fmt_header)
+impure elemental subroutine foo_df(nacf, outu, xstr)
 integer          , intent(in) :: nacf
 integer          , intent(in), optional :: outu
-character (len=*), intent(in), optional :: fmt_header
-if (present(fmt_header)) print*,"(4) in foo_df, fmt_header = '" // trim(fmt_header) // "'"
+character (len=*), intent(in), optional :: xstr
+if (present(xstr)) print*,"(4) in foo_df, xstr = '" // trim(xstr) // "'"
 end subroutine foo_df
 
-end module dataframe_stats_mod
+end module m2_mod
 
 program xdataframe_stats
-use dataframe_stats_mod, only: foo
+use m2_mod, only: foo
 implicit none
 print*,"calling foo with no title"
-call foo(nacf = 5, fmt_header="()")
+call foo(nacf = 5, xstr="()")
 end program xdataframe_stats
