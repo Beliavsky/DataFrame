319ea856804156faa49963298f634efcc527261f
Beliavsky <beliavsky@aol.com>
2025-04-05 12:34:45 -0400

    reduced code, still demonstrates bug

--- x1bug.f90
+++ x1bug.f90
@@ -1,42 +1,42 @@
 module basic_stats_mod
 implicit none
 private
-public :: print_acf
-interface print_acf
-   module procedure print_acf_mat
-end interface print_acf
+public :: foo
+interface foo
+   module procedure foo_mat
+end interface foo
 contains
 
-subroutine print_acf_mat(x, nacf, label)
+subroutine foo_mat(x, nacf, label)
 real, intent(in) :: x(:,:)
 integer, intent(in) :: nacf
 character (len=*), intent(in), optional :: label
-end subroutine print_acf_mat
+end subroutine foo_mat
 
 end module basic_stats_mod
 
 module dataframe_stats_mod
-use basic_stats_mod, only: print_acf
+use basic_stats_mod, only: foo
 implicit none
 private
-public :: print_acf
-interface print_acf
-  module procedure print_acf_df
-end interface print_acf
+public :: foo
+interface foo
+  module procedure foo_df
+end interface foo
 contains
 
-impure elemental subroutine print_acf_df(nacf, outu, fmt_header)
+impure elemental subroutine foo_df(nacf, outu, fmt_header)
 integer          , intent(in) :: nacf
 integer          , intent(in), optional :: outu
 character (len=*), intent(in), optional :: fmt_header
-if (present(fmt_header)) print*,"(4) in print_acf_df, fmt_header = '" // trim(fmt_header) // "'"
-end subroutine print_acf_df
+if (present(fmt_header)) print*,"(4) in foo_df, fmt_header = '" // trim(fmt_header) // "'"
+end subroutine foo_df
 
 end module dataframe_stats_mod
 
 program xdataframe_stats
-use dataframe_stats_mod, only: print_acf
+use dataframe_stats_mod, only: foo
 implicit none
-print*,"calling print_acf with no title"
-call print_acf(nacf = 5, fmt_header="()")
+print*,"calling foo with no title"
+call foo(nacf = 5, fmt_header="()")
 end program xdataframe_stats
