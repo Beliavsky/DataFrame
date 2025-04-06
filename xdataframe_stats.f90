f405b8c229f9c44082527dfea6d185b78d25c8df
Beliavsky <beliavsky@aol.com>
2025-04-05 21:26:12 -0400

    added debugging code

--- xdataframe_stats.f90
+++ xdataframe_stats.f90
@@ -1,9 +1,11 @@
 program xdataframe_stats
-use       dataframe_mod, only: DataFrame, print_summary, operator(*)
-use dataframe_stats_mod, only: simple_ret, cor
+use            kind_mod, only: dp
+use       dataframe_mod, only: DataFrame, print_summary, operator(*), &
+                               operator(**)
+use dataframe_stats_mod, only: simple_ret, cor, print_acf
 use     table_stats_mod, only: basic_stats_table, corr_table
 use           table_mod, only: Table, display
-use kind_mod, only: dp
+use basic_stats_mod, only: bug
 implicit none
 character (len=*), parameter :: data_file = "spy_efa_eem_tlt.csv"
 type(DataFrame) :: df, df_ret
@@ -18,4 +20,20 @@ call display(ret_stats, fmt_trailer="()")
 call display(corr_table(df_ret%values,df_ret%columns), &
    title="return correlations")
 call display(cor(df_ret))
+call print_acf(df_ret, nacf=5)
+call print_acf(df_ret**2, nacf=5)
+print*,"calling bug, fmt_header = '()'" ! debug
+call bug(df_ret%values, nacf=5, fmt_header="()")
+print*,"calling print_acf with no title"
+call print_acf(df_ret, nacf=5, fmt_header="()")
+stop "here"
+print*,"calling print_acf with no title, powers [1, 2]"
+call print_acf(df_ret**[1,2], nacf=5, fmt_header="()")
+stop "here (1)"
+print*,"calling print_acf(df_ret**[1,2], fmt_header = '()')" ! debug
+call print_acf(df_ret**[1,2], nacf=5, title=["ret   ","ret**2"], &
+   fmt_header="()")
+stop "here (2)"
+call print_acf(df_ret**[1,2], nacf=5, title=["ret   ","ret**2"], &
+   fmt_header="('abc')")
 end program xdataframe_stats
