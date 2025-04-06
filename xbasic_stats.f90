505bf0e70ee3f066f726d92cb70b4d756793b531
Beliavsky <beliavsky@aol.com>
2025-04-05 20:47:56 -0400

    Enhance basic_stats example with more features and improved formatting.

--- xbasic_stats.f90
+++ xbasic_stats.f90
@@ -5,22 +5,25 @@ use basic_stats_mod, only: mean, sd, variance, mean_and_sd, correl, &
                            moving_average, print_corr_mat, print_cov_mat
 use      random_mod, only: random_normal
 use        util_mod, only: display
+implicit none
 integer, parameter :: n = 10**6, ncol=3
 real(kind=dp) :: x(n), x1(n), x2(n), xmat(n, ncol), xran(3, 4), &
    xcorr(ncol, ncol)
 real(kind=dp), allocatable :: y(:), ysum(:), yma(:)
-integer :: k
+integer :: icol, jcol, k
 logical, parameter :: test_moving_sum_average = .true.
-x = 3.0_dp + 10.0_dp*random_normal(n)
-print "(*(a12))", "mean", "sd", "variance", "mean", "sd", "rms"
+real(kind=dp), parameter :: c0 = 3.0_dp, c1 = 10.0_dp
+x = c0 + c1*random_normal(n)
+print "('c1, c2 = ',2f10.4)", c0, c1
+print "(/,*(a12))", "mean", "sd", "variance", "mean", "sd", "rms"
 print "(*(f12.6))", mean(x), sd(x), variance(x), mean_and_sd(x), rms(x)
 print "(*(f12.6))", stats([character(len=8) :: "mean", "sd", "variance"], x)
 x1 = random_normal(n)
 x2 = x1 + random_normal(n)
-print*,"correlation:", correl(x1, x2)
+print "(/,a, f8.4)", "correlation of z1, z1+z2: ", correl(x1, x2)
 call random_number(xran)
-call display(xran, fmt_header="(/,'random matrix')")
-call display(xran, fmt_r="(*(1x,f6.2))")
+call display(xran, fmt_header="(/,'random matrix')", fmt_trailer="()")
+call display(xran, fmt_r="(*(1x,f6.2))", title='fmt_r="(*(1x,f6.2))"')
 xmat = random_normal(n, 3)
 xmat(:,3) = xmat(:,3) + xmat(:,1) + 2*xmat(:,2)
 call display(corr_mat(xmat), fmt_header="(/,'correlation matrix')")
@@ -30,16 +33,6 @@ do icol=1,ncol
    end do
 end do
 call display(xcorr, fmt_header="(/,'check correlation matrix')")
-if (test_moving_sum_average) then
-   y = [10, 20, 30, 40]
-   print "(/,*(f8.2))", y
-   do k=0,5
-      ysum = moving_sum(y, k)
-      yma  = moving_average(y, k)
-      print "('sum', i2, *(f8.2))", k, ysum
-      print "('avg', i2, *(f8.2))", k, yma
-   end do
-end if
 call print_corr_mat(xmat, col_names=["a", "b", "c"], &
    fmt_header="(/,'correlations')", fmt_trailer="('end', /)")
 call print_corr_mat(xmat, col_names=["a", "b", "c"], &
@@ -48,4 +41,15 @@ call print_corr_mat(xmat, col_names=["a", "b", "c"], &
 call print_cov_mat(xmat, col_names=["a", "b", "c"], &
    fmt_header="(/,'covariances')", fmt_col_names="(*(a6,:,1x))", &
    fmt_row="(a6, *(1x,f6.2))")
+
+if (test_moving_sum_average) then
+   y = [10, 20, 30, 40]
+   print "(/,'y = ',*(f8.2))", y
+   do k=0,5
+      ysum = moving_sum(y, k)
+      yma  = moving_average(y, k)
+      print "('sum', i2, *(f8.2))", k, ysum
+      print "('avg', i2, *(f8.2))", k, yma
+   end do
+end if
 end program xbasic_stats
